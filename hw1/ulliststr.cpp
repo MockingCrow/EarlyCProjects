df60617c21ba1dc7ff44394e458413604f98f193
Ignatius Mullin <imullin@usc.edu>
2021-01-28 04:42:45 +0000

    fixed then completed tests for getValAtLoc and push_back

--- hw1/ulliststr.cpp
+++ hw1/ulliststr.cpp
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
 
 using namespace std;
 
@@ -64,6 +65,7 @@ void ULListStr::clear()
   size_ = 0;
 }
 
+
 string* ULListStr::getValAtLoc(size_t loc) const
 {
   string* ptr;
@@ -73,7 +75,7 @@ string* ULListStr::getValAtLoc(size_t loc) const
 
   while (count != loc) //while count doesn't equal the ith element
   {
-    if (i == current->last)
+    if (i == ARRSIZE-1)
     {
       //moving to the next item when i reaches the end of the current item
       current = current->next;
@@ -90,6 +92,7 @@ string* ULListStr::getValAtLoc(size_t loc) const
   return ptr;
 }
 
+
 void ULListStr::push_back(const std::string& val)
 {
   Item* current = tail_;
@@ -99,44 +102,87 @@ void ULListStr::push_back(const std::string& val)
     //if the list is empty create a new item and
     //add the value the front of that item's array
     struct Item* newItem = (struct Item*) malloc(sizeof(struct Item));
-    size_ = 1;
+
     head_ = newItem;
     tail_ = newItem;
+
     newItem->first = 0;
     newItem->last = 1;
     newItem->val[0] = val;
 
+    size_ = 1;
   }
   else if (current->last == ARRSIZE)             
   {
     //if the last item is full add a new one  
     //and add that value at the front of the new item's array
     struct Item* newItem = (struct Item*) malloc(sizeof(struct Item));
+
+    current->next = newItem;
     newItem->prev = current;
     newItem->next = NULL;
     tail_ = newItem;
+
+    newItem->first = 0;
+    newItem->last = 1;
     newItem->val[0] = val;
+
     size_++;
+
   }
   else
   {
     //else just add it to the end of tail's array
-    int temp = current->last;
+    unsigned int temp = current->last;
     current->val[temp] = val;
-    current->last++;
+    (current->last)++;
+
+    size_++;
+
   }
 }
 
-/*void ULListStr::push_front(const std::string& val)
+
+void ULListStr::push_front(const std::string& val)
 {
-   Item* current = head_;
+  Item* current = head_;
 
-   if (current->first == 0)
-   {
-     struct Item* newItem = (struct Item*) malloc(sizeof(struct Item));
+  if (head_ == NULL && tail_ == NULL)
+  {
+    struct Item* newItem = (struct Item*) malloc(sizeof(struct Item));
+
+    head_ = newItem;
+    tail_ = newItem;
+
+    newItem->first = ARRSIZE-1;
+    newItem->last = ARRSIZE;
+    newItem->val[ARRSIZE-1] = val;
+
+    size_ = 1;
+  }
+  else if (current->first == 0)
+  {
+    struct Item* newItem = (struct Item*) malloc(sizeof(struct Item));
+
+    current->prev = newItem;
+    newItem->prev = NULL;
+    newItem->next = current;
+    head_ = newItem;
+
+    newItem->first = ARRSIZE-1;
+    newItem->last = ARRSIZE;
+    newItem->val[ARRSIZE-1] = val;
+
+    size_++;
+  }
+  else
+  {
+    unsigned int temp = current->first - 1;
+    current->val[temp] = val;
+    (current->first)--;
+
+    size_++;
+  }
 
 
-   }
-
-
-}*/
+}
--- hw1/ulliststr_test.cpp
+++ hw1/ulliststr_test.cpp
@@ -7,11 +7,29 @@ using namespace std;
 int main(int argc, char* argv[])
 {
   ULListStr test_list;
+  
+  /*
+  Testing push_back and get/getValAtLoc.
+  First populated a list greater that ARRSIZE then used
+  get to check that the list is created properly (including new Items)
+  */
+
   test_list.push_back("1");
   test_list.push_back("2");
   test_list.push_back("3");
+  test_list.push_back("4");
+  test_list.push_back("5");
+  test_list.push_back("6");
+  test_list.push_back("7");
+  test_list.push_back("8");
+  test_list.push_back("9");
+  test_list.push_back("10");
+  test_list.push_back("11");
+  test_list.push_back("12");
 
-  cout << test_list.get(0) << " " << test_list.get(1) << " " << test_list.get(2) << endl;
+  //change the indexes to whatever values you want to check
+  cout << test_list.get(0) << " " << test_list.get(10) << " "
+  << test_list.get(11) << endl; 
 
   return 0;
 }
