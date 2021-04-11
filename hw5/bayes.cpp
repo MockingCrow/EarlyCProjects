1074ef83235c642b579e03b70ac95237a13fe142
Ignatius Mullin <imullin@usc.edu>
2021-04-11 16:33:32 -0600

    done and dusted

--- hw5/bayes.cpp
+++ hw5/bayes.cpp
@@ -4,17 +4,22 @@
 #include <map>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    map<string, vector<string>*> fruits;
-    map<string, int> fruitSpread;
+    if (argc != 4)
+    {
+        cout << "Wrong number of Arguments" << endl;
+        return -1;
+    }
+    map<string, vector<string>*> fruits; //holds number of adjectives each fruit has, contains duplicate adjs
+    map<string, int> fruitSpread; //holds the number of occurances of each fruit
     map<string, vector<string>*>::iterator it;
-    vector<string> adj;
     string temp1;
-    int total;
+    int total = 0;
     int numFruits;
 
     ifstream train;
@@ -23,7 +28,7 @@ int main(int argc, char* argv[])
     if (!train.fail())
     {
         train >> numFruits;
-        getline(train, temp1);
+        getline(train, temp1); //takes the newline after the number of fruits
 
         for (int i = 0; i < numFruits; i++)
         {
@@ -35,7 +40,7 @@ int main(int argc, char* argv[])
             ss >> name;
             if (fruitSpread.find(name) == fruitSpread.end())
             {
-                fruitSpread.insert(make_pair(name, 1));
+                fruitSpread.insert(make_pair(name, 1)); //populate occurances of fruit
                 total++;
             }
             else
@@ -47,12 +52,12 @@ int main(int argc, char* argv[])
 
             if (fruits.find(name) == fruits.end())
             {
-                vector<string> *vec = new vector<string>;
+                vector<string> *vec = new vector<string>; //populate fruits
                 fruits.insert(make_pair(name, vec));
             }
             while (ss>>temp)
             {
-                it = fruits.find(name);
+                it = fruits.find(name); //populate array of adjs
                 it->second->push_back(temp);
             }
         }
@@ -64,6 +69,8 @@ int main(int argc, char* argv[])
 
     ifstream classify;
     classify.open(argv[2]);
+    ofstream output;
+    output.open(argv[3]);
     
     if (!classify.fail())
     {
@@ -71,25 +78,88 @@ int main(int argc, char* argv[])
         classify >> numLines;
         getline(classify, temp1);
 
-        for (int i = 0; i < numLine; i++)
+        for (int i = 0; i < numLines; i++)
         {   
+            double currMax = 0;
             string str;
             string temp;
+            string theFruit;
             getline(classify, str);
-            stringstream ss1(str);
-            while (ss1 >> temp)
-            {
-                
-            }
 
+            for (it = fruits.begin(); it != fruits.end(); it++)
+            {
+                stringstream ss1(str);
+                double numerator = 0;
+                double denominator = 0;
+                double answer = 0;
+                double egivenf = 1;
+                double probF;
+                double probnotF;
+                double egivennotf = 1;
+                while (ss1 >> temp)
+                {
+                    int num1 = 0;
+                    int num2 = 0;
+                    for (int i = 0; i < it->second->size(); i++)
+                    {
+                        if (it->second->at(i) == temp)
+                        {
+                            num1++;
+                        }
+                    }
+
+                    map<string,int>::iterator it1 = fruitSpread.find(it->first);
+                    egivenf = egivenf * (1+double(num1))/(1+double(it1->second));
+                    probF = double(it1->second)/double(total);
+                    probnotF = 1 - probF;
+
+                    map<string, vector<string>*>::iterator it2;
+                    for (it2 = fruits.begin(); it2 != fruits.end(); it2++)
+                    {
+                        if (it2->first != it->first)
+                        {
+                            for (int j = 0; j < it2->second->size(); j++)
+                            {
+                                if (it2->second->at(j) == temp)
+                                {
+                                    num2++;
+                                }   
+                            }
+                        }
+                    }
+                    egivennotf = egivennotf * (1+double(num2))/(1+double(total - it1->second));
+                }
+                numerator = egivenf * probF;
+                denominator = egivenf * probF + egivennotf * probnotF;
+                answer = numerator/denominator;
+                if (answer > currMax)
+                {
+                    currMax = answer;
+                    theFruit = it->first;
+                }
+            }
+            if (!output.fail())
+            {
+                output << theFruit << endl;
+            }
+            else
+            {
+                cout << "Failed to open output" << endl;
+            }
+            
         }
-        
-        ss1 >> 
     }
     else
     {
-
+        cout << "Failed to open classify" << endl;
     }
+    for (it = fruits.begin(); it != fruits.end(); it++)
+    {
+        delete it->second;
+    }
+    train.close();
+    classify.close();
+    output.close();
 
     return 0;
 }
